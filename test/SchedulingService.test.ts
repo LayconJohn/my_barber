0374a66c268f14d11df34eaf84b5860a1f2f909e
Laycon Gomes <laycon.gomes@tld.com.br>
2025-01-28 17:59:04 -0300

    test: insert baber client on case tests

--- test/SchedulingService.test.ts
+++ test/SchedulingService.test.ts
@@ -2,24 +2,28 @@ import SchedulingService from "../src/application/SchedulingService";
 
 test("Deve agendar um corte de cabelo", async () => {
     const schedulingService = new SchedulingService();
+    const baberClient = new Barberclient("Fulano");
     const date = new Date("2025-02-13T10:00:00");
-    const output = await schedulingService.schedule(date);
+    const output = await schedulingService.schedule(date, baberClient);
     expect(output).toStrictEqual({
-        message: "Corte agendado para 13/02/2025 às 10:00",
+        message: "Corte agendado para 13/02/2025 às 10:00 para o cliente Fulano",
         success: true
     });
 });
 
 test("Deve lançar um erro quando a data for fora do horário de expediente", async() => {
     const schedulingService = new SchedulingService();
+    const baberClient = new Barberclient("Fulano");
     const date = new Date("202-02-13T22:00:00");
-    await expect(() => schedulingService.schedule(date)).rejects.toThrow(new Error("Outside business hours"))
+    await expect(() => schedulingService.schedule(date, baberClient)).rejects.toThrow(new Error("Outside business hours"))
 })
 
 test("Deve lançar um erro quando a data já estiver agendada", async() => {
     const schedulingService = new SchedulingService();
     const dateScheduled = new Date("2025-02-13T10:00:00");
-    await schedulingService.schedule(dateScheduled);
+    const baberClient = new Barberclient("Fulano");
+    await schedulingService.schedule(dateScheduled, baberClient);
     const newScheduledDate = new Date("2025-02-13T10:00:00");
-    await expect(() => schedulingService.schedule(newScheduledDate)).rejects.toThrow(new Error("Date already scheduled"))
+    const newBarberClient = new Barberclient("Ciclano");
+    await expect(() => schedulingService.schedule(newScheduledDate, newBarberClient)).rejects.toThrow(new Error("Date already scheduled by Fulano"))
 });
