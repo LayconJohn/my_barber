565530f237ca0cc1a9936df5bd0ccacb14c084e2
Laycon <layconjohn.sg@gmail.com>
2025-03-16 04:56:01 -0300

    chore: prepare and update sql config

--- create.sql
+++ create.sql
@@ -1,21 +1,26 @@
+-- Criando o schema (se não existir)
 CREATE SCHEMA IF NOT EXISTS my_barber;
 
+-- Criando a tabela user
 CREATE TABLE IF NOT EXISTS my_barber.user (
-    user_id uuid primary key,
-    email text,
-    phone text,
-    name text,
+    user_id UUID PRIMARY KEY,
+    email TEXT,
+    phone TEXT,
+    name TEXT
 );
 
+-- Criando a tabela calendar
 CREATE TABLE IF NOT EXISTS my_barber.calendar (
-    calendar_id uuid primary key,
-    user_id uuid not null references my_barber.user (user_id),
-    date text,
-    time text,
-    start_date timestamp,
-    end_date timestamp,
-    duration int
+    calendar_id UUID PRIMARY KEY,
+    user_id UUID NOT NULL REFERENCES my_barber.user (user_id),
+    date TEXT,
+    time TEXT,
+    start_date TIMESTAMP,
+    end_date TIMESTAMP,
+    duration INT
 );
 
-insert into my_barber.user (user_id, email, name) values ('11111111-1111-1111-1111-111111111111', 'layconjohn@gmail.com', 'John', '5573988675742');
-insert into my_barber.user (user_id, email, name) values ('22222222-2222-2222-2222-222222222222', 'layconjohn.sg@gmail.com', 'Laycon', '5571999597588');
+-- Inserindo registros na tabela user
+INSERT INTO my_barber.user (user_id, email, phone, name) VALUES 
+    ('11111111-1111-1111-1111-111111111111', 'layconjohn@gmail.com', '5573988675742', 'John'),
+    ('22222222-2222-2222-2222-222222222222', 'layconjohn.sg@gmail.com', '5571999597588', 'Laycon');
