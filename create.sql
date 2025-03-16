5c84ac95ab5d4210e77f102d379cd6cb88fb0db2
Laycon <layconjohn.sg@gmail.com>
2025-03-16 04:48:33 -0300

    fix: update sintax on create aql

--- create.sql
+++ create.sql
@@ -1,14 +1,13 @@
-drop schema my_barber CASCADE;
-create schema my_barber
+CREATE SCHEMA IF NOT EXISTS my_barber;
 
-create my_barber.user {
+CREATE TABLE IF NOT EXISTS my_barber.user {
     user_id uuid primary key,
     email text,
     phone text,
     name text,
 }
 
-create my_barber.calendar {
+CREATE TABLE IF NOT EXISTS my_barber.calendar {
     calendar_id uuid primary key,
     user_id uuid not null references my_barber.user (user_id),
     date text,
@@ -18,5 +17,5 @@ create my_barber.calendar {
     duration int
 }
 
-insert into john.user (user_id, email, name) values (1, 'layconjohn@gmail.com', 'John', '5573988675742');
-insert into john.user (user_id, email, name) values (2, 'layconjohn.sg@gmail.com', 'Laycon', '5571999597588');
+insert into my_barber.user (user_id, email, name) values ('11111111-1111-1111-1111-111111111111', 'layconjohn@gmail.com', 'John', '5573988675742');
+insert into my_barber.user (user_id, email, name) values ('22222222-2222-2222-2222-222222222222', 'layconjohn.sg@gmail.com', 'Laycon', '5571999597588');
