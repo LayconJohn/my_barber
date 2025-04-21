6b05802e6768de94bc4865681a4a8038ec9d6c49
Laycon Gomes <laycon.gomes@tld.com.br>
2025-04-21 15:31:50 -0300

    test: insert calendarDAOPostres on ShcedulingService spec

--- test/SchedulingService.test.ts
+++ test/SchedulingService.test.ts
@@ -1,30 +1,29 @@
-import SchedulingService from "../src/application/SchedulingService";
+import SchedulingService from "../src/application/service/SchedulingService";
 import BarberClient from "../src/domain/BarberClient";
+import CalendarDAOPostgres from "../src/infra/DAO/CalendarDAOPostgres";
 
 test("Deve agendar um corte de cabelo", async () => {
-    const schedulingService = new SchedulingService();
+    const calendarDAOPosgres = new CalendarDAOPostgres()
+    const schedulingService = new SchedulingService(calendarDAOPosgres);
     const baberClient = new BarberClient("Fulano");
     const date = new Date("2025-02-13T10:00:00j");
-    const expectedOutput = {
-        date: "13/02/2025",
-        time: "10:00",
-        clientName: "Fulano",
-        avaiable: false
-    }
-    const output = await schedulingService.schedule(date, baberClient.getName());
+
+    await schedulingService.schedule(date, baberClient.getName());
     
-    expect(output).toStrictEqual(expectedOutput);
+    expect(calendarDAOPosgres.save).toHaveBeenCalled();
 });
 
 test("Deve lançar um erro quando a data for fora do horário de expediente", async() => {
-    const schedulingService = new SchedulingService();
+    const calendarDAOPostgres =  new CalendarDAOPostgres();
+    const schedulingService = new SchedulingService(calendarDAOPostgres);
     const baberClient = new BarberClient("Fulano");
     const date = new Date("2022-02-13T22:00:00");
     await expect(() => schedulingService.schedule(date, baberClient.getName())).rejects.toThrow(new Error("Outside business hours"))
 })
 
 test("Deve lançar um erro quando a data já estiver agendada", async() => {
-    const schedulingService = new SchedulingService();
+    const calendarDAOPosgres = new CalendarDAOPostgres();
+    const schedulingService = new SchedulingService(calendarDAOPosgres);
     const dateScheduled = new Date("2025-02-13T10:00:00");
     const baberClient = new BarberClient("Fulano");
     await schedulingService.schedule(dateScheduled, baberClient.getName());
